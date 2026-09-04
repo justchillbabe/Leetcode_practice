f40f8baf73f6752f4c9b3e5c79e24d6ca84a5752
Tanie_thelearner <ec24.tanisha.ghosh@stcet.ac.in>
2026-09-04 20:07:49 +0530

    Time: 2 ms (89.02%) | Memory: 48.2 MB (8.69%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int jump = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(i>jump){
+                return false;
+            }
+            jump = Math.max(jump, i+nums[i]);
+
+            if(jump==nums.length){
+                return true;
+            }
+        }
+        return true;
+    }
+}
